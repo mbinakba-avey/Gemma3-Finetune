36f542a2930a25604c7680dc45f85a482afbc574
mbinakba-avey <mbinakba-avey@gmail.com>
2025-12-17 23:48:11 +0000

    minor fix

--- scripts/finetune.sh
+++ scripts/finetune.sh
@@ -4,7 +4,7 @@ MODEL_NAME="google/gemma-3-4b-it"
 
 export PYTHONPATH=src:$PYTHONPATH
 export WANDB_API_KEY="7eadd40652b0651b0f12dc86ea4d5fde56db2e2a"
-export WANDB_PROJECT="gemma3-sft-4"
+export WANDB_PROJECT="gemma3-poc-3"
 
 # It is strongly recommended to train Gemma3 models with the `eager` attention implementation instead of `flash_attention_2`
 
@@ -12,7 +12,7 @@ deepspeed src/train/train_sft.py \
     --use_liger True \
     --deepspeed scripts/zero3.json \
     --model_id $MODEL_NAME \
-    --data_path data/train_set_sft.json \
+    --data_path data/train_set_SFT_21_conditions_generated_notes.json \
     --image_folder /path/to/your/image/folder \
     --disable_flash_attn2 True \
     --lora_enable False \
@@ -23,7 +23,7 @@ deepspeed src/train/train_sft.py \
     --output_dir output/test \
     --num_train_epochs 2 \
     --per_device_train_batch_size 1 \
-    --gradient_accumulation_steps 16 \
+    --gradient_accumulation_steps 1 \
     --learning_rate 1e-5 \
     --projector_lr 1e-5 \
     --vision_lr 2e-6 \
@@ -37,6 +37,6 @@ deepspeed src/train/train_sft.py \
     --report_to wandb \
     --lazy_preprocess True \
     --save_strategy "epoch" \
-    --save_epochs 1 \
+    --save_steps 1 \
     --save_total_limit 10 \
     --dataloader_num_workers 64
---
+++ scripts/store_2_aws.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+EPOCH_NUM=1
+CHECKPOINT_NUM=$((EPOCH_NUM * 7))
+MODEL_PATH="/root/Gemma3-Finetune/output/test/checkpoint-$CHECKPOINT_NUM"
+aws s3 cp --recursive --exclude "*.pt" $MODEL_PATH \
+    s3://avey-migration-bucket/tasnim/gemma-poc-3/epoch-$EPOCH_NUM/checkpoint-$CHECKPOINT_NUM/
+
+if [ $? -eq 0 ]; then
+    echo "Checkpoint $CHECKPOINT_NUM stored successfully"
+    rm -rf $MODEL_PATH
+else
+    echo "Checkpoint $CHECKPOINT_NUM storage failed"
+fi
