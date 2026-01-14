128c47b2c45698b1c1cd36f93cd153e2e962bdfe
mbinakba-avey <mbinakba-avey@gmail.com>
2026-01-14 14:15:36 +0000

    add generate script for sft specific icd10

---
+++ data/create_sft_specific.py
@@ -0,0 +1,28 @@
+import pandas as pd
+
+import json
+
+df = pd.read_csv('sft_codes_60_pure.csv')
+sft_data = []
+
+for index, row in df.iterrows():
+    _id = ("0" * 12)[:12-len(str(index))] + str(index)
+    code = row['code']
+    text = row['text']
+    conversations = [
+        {
+            "from": "human",
+            "value": "What is the ICD-10CM code for '{}'?".format(text)
+        },
+        {
+            "from": "gpt",
+            "value": f"{code}"
+        }
+    ]
+    sft_data.append({
+        "id": _id,
+        "conversations": conversations
+    })
+
+with open('train_set_specific_sft.json', 'w') as f:
+    json.dump(sft_data, f, indent=4)
