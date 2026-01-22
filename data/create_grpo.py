c289ab5eadc2d28d56ef0093c694ff9257cc5ed1
mbinakba-avey <mbinakba-avey@gmail.com>
2026-01-22 05:10:44 +0000

    fix

--- data/create_grpo.py
+++ data/create_grpo.py
@@ -2,16 +2,17 @@ import pandas as pd
 
 import json
 
-df = pd.read_csv('sft_codes_60_pure.csv')
+df = pd.read_csv('billable_codes.csv')
 sft_data = []
 
 for index, row in df.iterrows():
     _id = ("0" * 12)[:12-len(str(index))] + str(index)
     code = row['code']
     # add period after 3rd character in case len(code) > 3
-    if len(code) > 3:
+    if len(code) > 3 and '.' not in code:
         code = code[:3] + '.' + code[3:]
-    text = row['description']
+    text = row.get('description', row.get('text', ''))
+    assert text, f"Text is empty for code {code}"
     conversations = [
         {
             "from": "human",
