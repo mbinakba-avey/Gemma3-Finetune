c67673251821362bb88871cfb1f40dfa0e96d47a
mbinakba-avey <mbinakba-avey@gmail.com>
2026-01-24 00:39:50 +0000

    wip grpo

--- scripts/finetune_grpo.sh
+++ scripts/finetune_grpo.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 MODEL_NAME="google/gemma-3-4b-it"
+# MODEL_NAME="google/gemma-3-4b-pt"
 
 export PYTHONPATH=src:$PYTHONPATH
 export WANDB_API_KEY="7eadd40652b0651b0f12dc86ea4d5fde56db2e2a"
-export WANDB_PROJECT="gemma3-grpo-poc-3"
+export WANDB_PROJECT="gemma3-grpo-experiment-1-test"
 
 
 deepspeed src/train/train_grpo.py \
+    --loss_type "grpo" \
     --optim adamw_bnb_8bit \
     --max_completion_length 256 \
     --max_prompt_length 512 \
@@ -15,16 +17,16 @@ deepspeed src/train/train_grpo.py \
     --model_id $MODEL_NAME \
     --data_path data/train_set_specific_grpo.json \
     --image_folder /path/to/your/image/folder \
-    --disable_flash_attn2 True \
+    --disable_flash_attn2 False \
     --lora_enable False \
     --freeze_projector False \
     --freeze_vision_tower True \
     --freeze_llm False \
     --bf16 True \
     --output_dir output/test \
-    --num_train_epochs 3 \
-    --num_generations 4 \
-    --per_device_train_batch_size 192 \
+    --num_train_epochs 5 \
+    --num_generations 2 \
+    --per_device_train_batch_size 32 \
     --gradient_accumulation_steps 1 \
     --learning_rate 1e-5 \
     --projector_lr 1e-5 \
@@ -37,7 +39,7 @@ deepspeed src/train/train_grpo.py \
     --gradient_checkpointing True \
     --report_to wandb \
     --lazy_preprocess True \
-    --save_strategy "epoch" \
-    --save_steps 1 \
+    --save_strategy "steps" \
+    --save_steps 100 \
     --save_total_limit 10 \
-    --dataloader_num_workers 64
+    --dataloader_num_workers 16
